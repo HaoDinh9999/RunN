99e461ac15acaea63760fc8bb880bd3cc910b0e6
Hào Đinh Việt <vhao1509@gmail.com>
2022-12-08 08:08:48 +0700

    Transfer to wallet

--- frontend/screens/Wallet/Budget.screen.tsx
+++ frontend/screens/Wallet/Budget.screen.tsx
@@ -8,12 +8,19 @@ import SpendingScreen from "./Spending/Spending.screen";
 import { useWalletConnect } from "@walletconnect/react-native-dapp";
 import Web3 from "web3";
 import imagePath from "../../constant/imagePath";
+import WalletConnectProvider from "@walletconnect/web3-provider";
+import { Contract, ethers } from "ethers";
+import { RunnSneakerABI } from "../../constant/RunnSneakerABI";
+import { RunnMarketplaceABI } from "../../constant/RunnMarketplaceABI";
+import { mapTokenDataToSneakerInDetail } from "../../utils/formatTokenData";
 
 const BudgetScreen = () => {
     const navigation = useNavigation();
     const [tabActive, setTabActive] = useState(0);
     const [isShowModal, setIsShowModal] = useState(false);
-    const [addressWallet,setAddressWallet] = useState("0x0e23qwreqwrqqwrwrqwrqwrqwrwqr12123");
+    const [addressWallet, setAddressWallet] = useState("0x0e23qwreqwrqqwrwrqwrqwrqwrwqr12123");
+    const [sneakers, setSneakers] = useState([]);
+
     const connector = useWalletConnect();
     const handleActionShow = () => {
         setIsShowModal(true);
@@ -29,6 +36,109 @@ const BudgetScreen = () => {
         setTabActive(1);
         setIsShowModal(true);
     }
+    const fetchAllSneakers = async () => {
+
+        try {
+            // const ethereum  = this.provider.ethereum;
+            // const provider = new WalletConnectProvider({
+            //   infuraId: "27e484dcd9e3efcfd25a83a78777cdf1",
+            // });
+            //   const ethereumProvider = new WalletConnectProvider({
+            //     rpc: {
+            //         56: 'https://bsc-dataseed1.binance.org:443',
+            //     },
+            //     chainId: 56,
+            //     connector: connector,
+            //     qrcode: false,
+            // });
+            // const ethereumProvider = new WalletConnectProvider({
+            //   rpc: {
+            //     11155111: "https://rpc.sepolia.dev",
+            //     3: "https://ropsten.mycustomnode.com",
+            //     100: "https://dai.poa.network",
+            //     // ...
+            //   },
+            // });
+            const ethereumProvider = new WalletConnectProvider({
+                rpc: {
+                    11155111: "https://rpc.sepolia.dev",
+                },
+                chainId: 11155111,
+                connector: connector,
+                qrcode: false,
+            });
+            const provider = new ethers.providers.Web3Provider(ethereumProvider);
+
+
+            const signer = provider.getSigner();
+            console.log("Signer", signer);
+            try {
+                const accounts = await ethereumProvider?.request({ method: 'eth_requestAccounts' })
+                console.log("Accounts", accounts)
+            } catch (e) {
+                console.error(e)
+            }
+
+            const marketplaceContract = new Contract(
+                '0xb8f25b2ed468d2144B2Dcf75D5db7400728AE4e2',
+                RunnMarketplaceABI,
+                signer
+            );
+            const nftContract = new Contract(
+                '0x4a30Cf2843f8075e6aa92e867c38E8308bA7b998',
+                RunnSneakerABI,
+                signer
+            );
+            const res =
+                await marketplaceContract.functions.sellInfoActiveByContract(
+                    '0x4a30Cf2843f8075e6aa92e867c38E8308bA7b998'
+                );
+            const allSellInfos = res[0];
+
+            const formattedSneakers = allSellInfos.map(async (sellInfo) => {
+                const { price, tokenId, saleId, seller } = sellInfo;
+
+                const tokenData = await nftContract.tokenData(tokenId);
+                return {
+                    ...mapTokenDataToSneakerInDetail(tokenData),
+                    id: tokenId,
+                    saleId,
+                    seller,
+                    price,
+                };
+            });
+            const resultSneakers = await Promise.all(formattedSneakers);
+            setSneakers(resultSneakers);
+            console.log("resultSneakers", resultSneakers)
+
+            //   const connector = useWalletConnect();
+            //   const provider = new WalletConnectProvider({
+            //   // rpc: {
+            //   //     56: 'https://bsc-dataseed1.binance.org:443',
+            //   // },
+            //   // chainId: 56,
+            //   // connector: connector,
+            //   // qrcode: false,
+            // });
+            //   await provider.enable();
+            // const ethers_provider = new ethers.providers.Web3Provider(provider);
+            // const signer = ethers_provider.getSigner();
+            // console.log("Signer ", signer)
+            //   try {
+            //     await provider?.request({ method: 'eth_requestAccounts'})
+            //   } catch(e) {
+            //     console.error(e)
+            //   }
+            // } else {
+            //   console.error('Please install MetaMask')
+            // }
+
+
+        }
+        catch (err) {
+            console.log(err);
+        }
+    };
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -40,16 +150,16 @@ const BudgetScreen = () => {
                 </View>
             </View>
             <View style={styles.addressContain} >
-            {
-                connector.connected 
-                ? <Text color={colors.lightGray2}  fontSize="md" onPress={handleActionShow}>{connector?.accounts[0]}</Text>
-                :<Text color={colors.lightGray2}  fontSize="md" onPress={handleBtnConnect}>Connect wallet</Text>
-            }
-                
+                {
+                    connector.connected
+                        ? <Text color={colors.lightGray2} fontSize="md" onPress={handleActionShow}>{connector?.accounts[0]}</Text>
+                        : <Text color={colors.lightGray2} fontSize="md" onPress={handleBtnConnect}>Connect wallet</Text>
+                }
+
 
             </View>
             <View style={styles.coinContain}>
-                <View style={{ flexDirection: 'row', alignItems:'center' }}>
+                <View style={{ flexDirection: 'row', alignItems: 'center' }}>
                     <Image size={7} borderRadius={100} source={imagePath.coin} alt="Alternate Text" />
                     <Text color={colors.coin} bold fontSize="md" marginLeft={4}>Earned AMT</Text>
 
@@ -66,6 +176,7 @@ const BudgetScreen = () => {
                 <Text color={colors.white} bold fontSize="lg">0</Text>
 
             </View>
+            <Button onPress={fetchAllSneakers}>Click here</Button>
             {/* <Divider my="2" _light={{
                 bg: colors.background.progress
             }} _dark={{
